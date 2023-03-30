3cba7ea1129895e28b4687e2fc535dfd8691ce7e
Andre-se <ibrahimtanriover64@gmail.com>
2023-03-30 05:48:53 +0200

    v1

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { Box, useTheme } from "@mui/material";
+import { useState } from "react";
+import { tokens } from "../../theme";
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}></Box>
+  );
+};
+
+export default Sidebar;
